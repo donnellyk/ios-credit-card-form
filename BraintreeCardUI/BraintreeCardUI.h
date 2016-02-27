fc70f11c78c00d45fe3ce5d5679a62558ff04da7
Kevin <kevin@kvnd.me>
2016-02-27 09:17:35 -0600

    Add Framework

---
+++ BraintreeCardUI/BraintreeCardUI.h
@@ -0,0 +1,19 @@
+//
+//  BraintreeCardUI.h
+//  BraintreeCardUI
+//
+//  Created by Kevin Donnelly on 2/26/16.
+//  Copyright © 2016 Braintree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BraintreeCardUI.
+FOUNDATION_EXPORT double BraintreeCardUIVersionNumber;
+
+//! Project version string for BraintreeCardUI.
+FOUNDATION_EXPORT const unsigned char BraintreeCardUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BraintreeCardUI/PublicHeader.h>
+
+
