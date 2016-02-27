bd867a2ce89e325bf20836a4d9d4add9bfc6473c
Kevin <kevin@kvnd.me>
2016-02-27 09:55:25 -0600

    Update deployment target

--- BraintreeCardUI/BraintreeCardUI.h
+++ BraintreeCardUI/BraintreeCardUI.h
@@ -1,11 +1,3 @@
-//
-//  BraintreeCardUI.h
-//  BraintreeCardUI
-//
-//  Created by Kevin Donnelly on 2/26/16.
-//  Copyright © 2016 Braintree. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for BraintreeCardUI.
@@ -16,4 +8,58 @@ FOUNDATION_EXPORT const unsigned char BraintreeCardUIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BraintreeCardUI/PublicHeader.h>
 
-
+#import <BraintreeCardUI/BTUIPayPalMonogramCardView.h>
+#import <BraintreeCardUI/BTUIHorizontalButtonStackSeparatorLineView.h>
+#import <BraintreeCardUI/BTUISummaryView.h>
+#import <BraintreeCardUI/BTUITextField.h>
+#import <BraintreeCardUI/BTUICoinbaseWordmarkVectorArtView.h>
+#import <BraintreeCardUI/BTUI.h>
+#import <BraintreeCardUI/BTUIMasterCardVectorArtView.h>
+#import <BraintreeCardUI/BTMockApplePayPaymentAuthorizationViewController.h>
+#import <BraintreeCardUI/BTUIScrollView.h>
+#import <BraintreeCardUI/BTUICardHint.h>
+#import <BraintreeCardUI/BTUIAmExVectorArtView.h>
+#import <BraintreeCardUI/BTUIVenmoMonogramCardView.h>
+#import <BraintreeCardUI/BTUIVenmoWordmarkVectorArtView.h>
+#import <BraintreeCardUI/BTUICardExpirationValidator.h>
+#import <BraintreeCardUI/BTUIPayPalWordmarkCompactVectorArtView.h>
+#import <BraintreeCardUI/BTDropInErrorAlert.h>
+#import <BraintreeCardUI/BTUICardCvvField.h>
+#import <BraintreeCardUI/BTUIDinersClubVectorArtView.h>
+#import <BraintreeCardUI/BTUIPayPalCompactButton.h>
+#import <BraintreeCardUI/BTUIPayPalWordmarkVectorArtView.h>
+#import <BraintreeCardUI/BTUIFloatLabel.h>
+#import <BraintreeCardUI/BTUIPaymentButtonCollectionViewCell.h>
+#import <BraintreeCardUI/BTUICTAControl.h>
+#import <BraintreeCardUI/BTUICardVectorArtView.h>
+#import <BraintreeCardUI/BTUIVisaVectorArtView.h>
+#import <BraintreeCardUI/BTUIUtil.h>
+#import <BraintreeCardUI/BTDropInUtil.h>
+#import <BraintreeCardUI/BTUIHorizontalButtonStackCollectionViewFlowLayout.h>
+#import <BraintreeCardUI/BTUICVVBackVectorArtView.h>
+#import <BraintreeCardUI/BTUICardType.h>
+#import <BraintreeCardUI/BTUICardNumberField.h>
+#import <BraintreeCardUI/BTUIVenmoButton.h>
+#import <BraintreeCardUI/BTUIPayPalButton.h>
+#import <BraintreeCardUI/BTUIDiscoverVectorArtView.h>
+#import <BraintreeCardUI/BTUILocalizedString.h>
+#import <BraintreeCardUI/BTUIJCBVectorArtView.h>
+#import <BraintreeCardUI/BTUIPaymentOptionType.h>
+#import <BraintreeCardUI/BTUICardPostalCodeField.h>
+#import <BraintreeCardUI/BTUICoinbaseButton.h>
+#import <BraintreeCardUI/BTUICVVFrontVectorArtView.h>
+#import <BraintreeCardUI/BTUIVectorArtView.h>
+#import <BraintreeCardUI/BTUIViewUtil.h>
+#import <BraintreeCardUI/BTUIMaestroVectorArtView.h>
+#import <BraintreeCardUI/BTUIThemedView.h>
+#import <BraintreeCardUI/BTUIFormField_Protected.h>
+#import <BraintreeCardUI/BTUIPaymentMethodView.h>
+#import <BraintreeCardUI/BTUICardExpiryField.h>
+#import <BraintreeCardUI/BTMockApplePayPaymentAuthorizationView.h>
+#import <BraintreeCardUI/UIColor+BTUI.h>
+#import <BraintreeCardUI/BTUIUnknownCardVectorArtView.h>
+#import <BraintreeCardUI/BTUICoinbaseMonogramCardView.h>
+#import <BraintreeCardUI/BTDropInLocalizedString.h>
+#import <BraintreeCardUI/BTUIFormField.h>
+#import <BraintreeCardUI/BTUICardFormView.h>
+#import <BraintreeCardUI/BTUICardExpiryFormat.h>
